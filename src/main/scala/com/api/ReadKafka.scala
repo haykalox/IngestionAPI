190ec8fa6251aad01a40f5793d88b421022f04ab
haykalox <haykalox@gmail.com>
2021-10-20 10:25:49 +0100

    Update ReadKafka.scala

--- src/main/scala/com/api/ReadKafka.scala
+++ src/main/scala/com/api/ReadKafka.scala
@@ -1,6 +1,7 @@
 package com.api
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{ForeachWriter, Row, SparkSession}
+
 
 object ReadKafka {
   def main(args: Array[String]): Unit = {
@@ -11,18 +12,42 @@ object ReadKafka {
       .enableHiveSupport()
       .getOrCreate()
 
+
+
     val df = spark.readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "0.0.0.0:6667")
       .option("subscribe", "streaming-test")
       .load()
 
-    val newDF = df.selectExpr("CAST(value AS STRING)")
+    val data = df.selectExpr("CAST(value AS STRING)")
+/*
+    data.writeStream
+      .format("text")
+      .outputMode("append")
+      .option("checkpointLocation", "/home/datakafka/delete")
+      .option("path","/home/datakafka/test_kafka")
+      .start()
+      .awaitTermination()
+*/
 
-    val query = newDF.writeStream
+    val output_stream_df = data.writeStream.format("hive")
+      .queryName("hiveSink")
+      .option("database", "default")
+      .option("table", "kafkaTest")
+      .option("checkpointLocation", "/home/datakafka/test_kafka")
+      .option("spark.acid.streaming.log.metadataDir", "/home/datakafka/test_kafka/spark-acid")
+       .option("metastoreUri", "thrift://hive-metastore:9083")
+      .start()
+
+    output_stream_df.awaitTermination()
+
+
+/*
+    val query = df.writeStream
       .outputMode("append")
       .format("console")
       .start()
-    query.awaitTermination()
+    query.awaitTermination()*/
   }
 }
